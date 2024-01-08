5a665e764ea4b97539841716aa39275158ee43b8
juan Manuel Gaitan <jmgaitan93@gmail.com>
2024-01-08 17:22:18 -0300

    iniciando el punto 12

---
+++ main.js
@@ -0,0 +1,8 @@
+let page = document.body;
+let tematicPage = document.createElement('span');
+tematicPage.textContent = ' Temática elegida: SPA ';
+page.append(tematicPage);
+
+let iconPage = document.createElement('img');
+iconPage.getAttribute('src', 'https://static.educacionit.com/educacionit/assets/imagotype-it-fill-v2-color.svg')
+page.append(iconPage);
